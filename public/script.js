6e03156f6c0605a43af09f22f2eb98d79c2de7ae
hana-bananaa <debartosiaki@gmail.com>
2021-07-16 21:08:05 -0300

    finalizando TUDO uhul!

--- public/script.js
+++ public/script.js
@@ -1,4 +1,13 @@
 // grab everything we need
+let nome = document.querySelector('#name')
+let email = document.querySelector('#email')
+let duvida = document.querySelector('#duvida')
+let labelnome = document.querySelector('#labelnome')
+let labelemail = document.querySelector('#labelemail')
+let labelduvida = document.querySelector('#labelduvida')
+let validnome = false;
+let validemail = false;
+
 const btn = document.querySelector("button.mobile-menu-button");
 const menu = document.querySelector(".mobile-menu");
 
@@ -6,3 +15,47 @@ const menu = document.querySelector(".mobile-menu");
 btn.addEventListener("click", () => {
   menu.classList.toggle("hidden");
 });
+
+function enviar(){
+  if(validnome ==true && validemail ==true){
+    let listaUser = JSON.parse(localStorage.getItem('listaUser') || '[]')
+
+    listaUser.push(
+      {
+        nome: nome.value,
+        email: email.value
+      }
+      )
+      localStorage.setItem('listaUser', JSON.stringify(listaUser))
+      alert('sonho enviado')
+      setTimeout(()=>{
+        window.location.href = 'https://github.com/hana-bananaa'
+      }, 3000)
+  
+    
+  }
+  else {
+    alert('preencha os dados')
+  }
+}
+
+
+nome.addEventListener('keyup', () => {
+  if(nome.value.length <=2){
+    labelnome.setAttribute('style','color:red')
+    validnome = false;
+} else {
+  labelnome.setAttribute('style','color:green')
+  validnome = true;
+}
+})
+
+email.addEventListener('keyup', () => {
+  if(email.value.length <=2){
+    labelemail.setAttribute('style','color:red')
+    validemail = false;
+} else {
+  labelemail.setAttribute('style','color:green')
+  validemail = true;
+}
+})
